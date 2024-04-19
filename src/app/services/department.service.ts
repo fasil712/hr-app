18aba712e75871c606077871ae71406b3e64646d
Fasil Getie <fasilgetie12@gmail.com>
2024-04-19 20:21:30 +0300

    new update

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
-import { environment } from '../environments/environment';
 import { ApiPaths } from '../others/api-paths.enum';
 import { Department } from '../models/department';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
-import { environment } from '../environments/environment';
 import { ApiPaths } from '../others/api-paths.enum';
 import { Employee } from '../models/employee';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
